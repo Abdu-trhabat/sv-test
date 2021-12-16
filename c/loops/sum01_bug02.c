af25b9405fdedf38655dd609dcf0d7632c7b907f
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2021-12-16 01:32:15 +0100

    fix two unsigned integer overflows
    * line 24: For n===INT_MAX in the apparently last iteration,
      the value of i is also INT_MAX but incremented at the end of the loop
      and the behavior after this overflow is undefined.
    * line 29: n is multiplied by 2
    
    Idea of this fix:
    * I would like to change as few as possible.
    * The second overflow can be avoided by a cast to long long.
    * The first overflow can be avoided by the assumption that
      n is different from INT_MAX.
    
    See discussion
    https://gitlab.com/sosy-lab/sv-comp/archives-2022/-/issues/43

--- c/loops/sum01_bug02.c
+++ c/loops/sum01_bug02.c
@@ -12,10 +12,13 @@ void __VERIFIER_assert(int cond) {
 extern unsigned int __VERIFIER_nondet_uint();
 int main() { 
   int i, j=10, n=__VERIFIER_nondet_uint(), sn=0;
+  if (n==2147483647) {
+    return 0;
+  }
   for(i=1; i<=n; i++) {
     if (i<j) 
     sn = sn + a;
     j--;
   }
-  __VERIFIER_assert(sn==n*a || sn == 0);
+  __VERIFIER_assert(sn==((long long) n)*a || sn == 0);
 }
