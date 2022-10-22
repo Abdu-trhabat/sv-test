5ebb87ee81a3fb1d96237645e82113cc264ec84e
Simmo Saan <simmo.saan@gmail.com>
2022-10-22 21:53:52 +0300

    Ignore ConcurrencySafety-NoOverflows in preprocessing CI

--- c/compare.py
+++ c/compare.py
@@ -32,6 +32,7 @@ TASKS_TO_IGNORE = {
 # categories to be excluded ... (with reason and debug information)
 CATEGORIES_TO_IGNORE = {
   "ConcurrencySafety-Main": "(platform-dependent types)",
+  "ConcurrencySafety-NoOverflows": "(platform-dependent types)",
   "NoDataRace-Main": "(platform-dependent types)",
   "SoftwareSystems-OpenBSD-MemSafety": "(only custom includes, no system headers, complicated build process)",
   "SoftwareSystems-SQLite-MemSafety": "(complicated build process, requires patched version of cilly)",
