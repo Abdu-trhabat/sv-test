07957160138e30ed1b1a7a1a18200aa9e665b290
Frank Schüssele <schuessf@informatik.uni-freiburg.de>
2022-10-26 16:19:51 +0200

    Fix termination-memory-alloca benchmarks

---
+++ c/termination-memory-alloca/BrockschmidtCookFuhs-2013CAV-Introduction-alloca-2.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <alloca.h>
+
+int main() {
+    int* x = alloca(sizeof(int));
+    int* y = alloca(sizeof(int));
+    *x = __VERIFIER_nondet_int();
+    *y = 1;
+    while (*x > 0) {
+        *x = *x - *y;
+        *y = *y + 1;
+    }
+    return 0;
+}
