03545733e36d4e360e7f4c01fc1ea9c7d1a1c22d
BaierD <daniel.baier@sosy.ifi.lmu.de>
2023-02-13 16:50:19 +0100

    Fix compiler warning for unspecified tasks

--- c/unspecified-runtime/pointer_larger_pointer_unsafe1-2.c
+++ c/unspecified-runtime/pointer_larger_pointer_unsafe1-2.c
@@ -5,13 +5,11 @@ extern void __assert_fail(const char *, const char *, unsigned int, const char *
 void reach_error() { __assert_fail("0", "pointer_larger_pointer_unsafe1-2", 3, "reach_error"); }
 
 int main() {
-  int* ptr0,ptr1,ptr2;
-
   // C11 ISO/IEC 9899:201x 7.22.3 states that: the order and contiguity of storage allocated by successive calls to the ..., malloc, ... functions is unspecified.
-  ptr0 = (int*) malloc(4096); 
-  ptr1 = (int*) malloc(10);
+  int * ptr0 = (int *) malloc(4096); 
+  int * ptr1 = (int *) malloc(10);
   free(ptr0);
-  ptr2 = (int*) malloc(4096);   
+  int * ptr2 = (int*) malloc(4096);   
   if (ptr1 > ptr2) {reach_error();abort();}
   return 0;
 }
--- c/unspecified-runtime/pointer_larger_pointer_unsafe1-2.i
+++ c/unspecified-runtime/pointer_larger_pointer_unsafe1-2.i
@@ -101,11 +101,10 @@ extern void abort(void);
 extern void __assert_fail(const char *, const char *, unsigned int, const char *) __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
 void reach_error() { __assert_fail("0", "pointer_larger_pointer_unsafe1-2", 3, "reach_error"); }
 int main() {
-  int* ptr0,ptr1,ptr2;
-  ptr0 = (int*) malloc(4096);
-  ptr1 = (int*) malloc(10);
+  int * ptr0 = (int *) malloc(4096);
+  int * ptr1 = (int *) malloc(10);
   free(ptr0);
-  ptr2 = (int*) malloc(4096);
+  int * ptr2 = (int*) malloc(4096);
   if (ptr1 > ptr2) {reach_error();abort();}
   return 0;
 }
