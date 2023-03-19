9d8efa1edb5893bad7bc585daef1bfc9cab6f319
BaierD <daniel.baier@sosy.ifi.lmu.de>
2023-03-19 10:41:22 +0100

    Add tasks with unspecified behavior based on side effects caused by memory manipulation

---
+++ c/unspecified-compile/assignment_order_unsafe2-2.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+extern void abort(void);
+extern void __assert_fail(const char *, const char *, unsigned int, const char *) __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
+void reach_error() { __assert_fail("0", "assignment_order_unsafe2-2.c", 3, "reach_error"); }
+
+
+int* f1(int * g) {
+  *g = 3;
+  return malloc(sizeof(int));
+}
+
+int f2(int * g) {
+  *g = 5;
+  return 7;
+}
+
+int main() {
+  int * g = malloc(sizeof(int));
+  // C11 ISO/IEC 9899:201x Annex J.1 Unspecified behaviour and 6.5.16 state that the order in which the operands of an assignment operator are evaluated is not specified 
+  *f1(g) = f2(g);
+  // g is either 3 or 5, depending on which function is evaluated last
+  if (*g == 5) {reach_error();abort();}
+  return 0;
+}
---
+++ c/unspecified-compile/function_execution_unsafe4-2.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+extern void abort(void);
+extern void __assert_fail(const char *, const char *, unsigned int, const char *) __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
+void reach_error() { __assert_fail("0", "function_execution_unsafe4-2.c", 3, "reach_error"); }
+
+int f1(int * intStorage) {
+  *intStorage = 3;
+  return 5;
+}
+
+int f2(int * intStorage) {
+  *intStorage = 5;
+  return 7;
+}
+
+int main() {
+  // C11 ISO/IEC 9899:201x Annex J.1 Unspecified behaviour states that the order in which subexpressions are evaluated is not specified (with some exceptions, + not being one)
+  int * intStorage = malloc(sizeof(int));
+  if (intStorage == 0) {
+    return 0;
+  }
+  int c;
+  c = f1(intStorage) + f2(intStorage);
+  if (*intStorage == 5) {reach_error();abort();}
+  return 0;
+}
