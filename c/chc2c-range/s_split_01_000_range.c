f5518d9824c0f072e5d62aaaafb30d3c3957860e
Levente Bajczi <bajczilevi@gmail.com>
2023-11-01 17:04:24 +0100

    Added CHC benchmarks

---
+++ c/chc2c-range/s_split_01_000_range.c
@@ -0,0 +1,9 @@
+// This file is part of the SV-Benchmarks collection of verification tasks:
+// https://gitlab.com/sosy-lab/benchmarking/sv-benchmarks
+//
+// SPDX-FileCopyrightText: 2023 Levente Bajczi, Department of Measurement and Information Systems, Budapest University of Technology and Economics
+//
+// SPDX-License-Identifier: Apache-2.0
+
+// Original CHC file: aeval-benchmarks/s_split_01_000.smt2
+
