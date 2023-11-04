7e8c94cbdc39e7fbae7dc2191bbbebd69c8fca76
Thomas Lemberger <lembergerth@gmail.com>
2023-11-04 10:20:33 +0100

    Let compare.py ignore new OpenBSD category as well
    In addition to old OpenBSD category

--- c/compare.py
+++ c/compare.py
@@ -36,6 +36,7 @@ CATEGORIES_TO_IGNORE = {
   "ConcurrencySafety-MemSafety": "(platform-dependent types)",
   "NoDataRace-Main": "(platform-dependent types)",
   "SoftwareSystems-OpenBSD-MemSafety": "(only custom includes, no system headers, complicated build process)",
+  "SoftwareSystems-OpenBSD-ReachSafety": "(only custom includes, no system headers, complicated build process)",
   "SoftwareSystems-SQLite-MemSafety": "(complicated build process, requires patched version of cilly)",
 }
 
