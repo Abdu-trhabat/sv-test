ca7706db1c9ddebbe114b3f5d6bf5e6273bc10a0
Simmo Saan <simmo.saan@gmail.com>
2023-11-07 12:54:26 +0200

    Fix merged SoftwareSystems-Other-MemSafety in compare.py

--- c/compare.py
+++ c/compare.py
@@ -35,7 +35,7 @@ CATEGORIES_TO_IGNORE = {
   "ConcurrencySafety-NoOverflows": "(platform-dependent types)",
   "ConcurrencySafety-MemSafety": "(platform-dependent types)",
   "NoDataRace-Main": "(platform-dependent types)",
-  "SoftwareSystems-OpenBSD-MemSafety": "(only custom includes, no system headers, complicated build process)",
+  "SoftwareSystems-Other-MemSafety": "(OpenBSD: only custom includes, no system headers, complicated build process)",
   "SoftwareSystems-SQLite-MemSafety": "(complicated build process, requires patched version of cilly)",
 }
 
