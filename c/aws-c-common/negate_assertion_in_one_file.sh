dbf117a63427264d21a8d85ed04cb61adb4679d0
nianzelee <nian-ze.lee@sosy.ifi.lmu.de>
2023-11-08 17:13:08 +0100

    Add REUSE header for scripts

--- c/aws-c-common/negate_assertion_in_one_file.sh
+++ c/aws-c-common/negate_assertion_in_one_file.sh
@@ -1,3 +1,10 @@
+# This file is part of the SV-Benchmarks collection of verification tasks:
+# https://github.com/sosy-lab/sv-benchmarks
+#
+# SPDX-FileCopyrightText: 2011-2020 The SV-Benchmarks Community
+#
+# SPDX-License-Identifier: Apache-2.0
+
 file="$1"
 filename="${file%.*}"
 new_name="${filename}_negated.i"
