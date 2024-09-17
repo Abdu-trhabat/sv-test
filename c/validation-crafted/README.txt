ce8250cf6833a1d7c0c3c4d32b34194a8436eed7
Paulína Ayaziová <paulina.ayazi@gmail.com>
2024-09-17 15:38:15 +0200

    Add copyright header to programs and update witnesses accordingly

--- c/validation-crafted/README.txt
+++ c/validation-crafted/README.txt
@@ -1,4 +1,9 @@
-These tasks were contributed by Paulína Ayaziová.
+# This file is part of the SV-Benchmarks collection of verification tasks:
+# https://gitlab.com/sosy-lab/benchmarking/sv-benchmarks
+#
+# SPDX-FileCopyrightText: 2024 Paulína Ayaziová
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 This directory contains 6 simple programs and 100 violation witnesses 
 in the witness format 2.0. The set is designed to verify that the validator
--- c/validation-crafted/while.c
+++ c/validation-crafted/while.c
@@ -1,3 +1,10 @@
+// This file is part of the SV-Benchmarks collection of verification tasks:
+// https://github.com/sosy-lab/sv-benchmarks
+//
+// SPDX-FileCopyrightText: 2024 Paulína Ayaziová
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 extern void __assert_fail(const char *, const char *, unsigned int, const char *) __attribute__ ((__nothrow__, __leaf__)) __attribute__ ((__noreturn__));
 void reach_error(){ __assert_fail("0", "while.c", 2, "reach_error"); }
 
