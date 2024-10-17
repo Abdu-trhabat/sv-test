04cbf6bfbb1cc683e82cbc1642d1d065bfcd568b
Hernan Ponce de Leon <zeta96@gmail.com>
2024-10-17 19:35:49 +0200

    Properly declare when funcitions do not take parameters

--- c/goblint-regression/28-race_reach_27-funptr_racing.c
+++ c/goblint-regression/28-race_reach_27-funptr_racing.c
@@ -12,21 +12,21 @@
 int global;
 pthread_mutex_t gm = PTHREAD_MUTEX_INITIALIZER;
 
-void bad() { 
+void bad(void) {
   access(global);
 }
 
-void good() { 
+void good(void) {
   pthread_mutex_lock(&gm);
   access(global);
   pthread_mutex_unlock(&gm);
 }
 
-void (*f)() = good;
+void (*f)(void) = good;
 pthread_mutex_t fm = PTHREAD_MUTEX_INITIALIZER;
 
 void *t_fun(void *arg) {
-  void (*g)();
+  void (*g)(void);
 
   pthread_mutex_lock(&fm);
   g = f;
