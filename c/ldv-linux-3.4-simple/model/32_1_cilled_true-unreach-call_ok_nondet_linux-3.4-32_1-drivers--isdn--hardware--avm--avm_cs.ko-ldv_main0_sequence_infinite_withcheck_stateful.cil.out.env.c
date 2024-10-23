2854999a3115c84595cf69ff6f3c028278789f44
Frank Schüssele <schuessf@informatik.uni-freiburg.de>
2024-10-23 15:25:44 +0200

    Add definitions for addcard functions

--- c/ldv-linux-3.4-simple/model/32_1_cilled_true-unreach-call_ok_nondet_linux-3.4-32_1-drivers--isdn--hardware--avm--avm_cs.ko-ldv_main0_sequence_infinite_withcheck_stateful.cil.out.env.c
+++ c/ldv-linux-3.4-simple/model/32_1_cilled_true-unreach-call_ok_nondet_linux-3.4-32_1-drivers--isdn--hardware--avm--avm_cs.ko-ldv_main0_sequence_infinite_withcheck_stateful.cil.out.env.c
@@ -10,6 +10,15 @@ int b1pcmcia_delcard(unsigned int arg0, unsigned int arg1) {
   // Simple type
   return __VERIFIER_nondet_int();
 }
+int b1pcmcia_addcard_b1(unsigned int port , unsigned int irq ) {
+  return __VERIFIER_nondet_int();
+}
+int b1pcmcia_addcard_m1(unsigned int port , unsigned int irq ) {
+  return __VERIFIER_nondet_int();
+}
+int b1pcmcia_addcard_m2(unsigned int port , unsigned int irq ) {
+  return __VERIFIER_nondet_int();
+}
 
 // Function: dev_err
 // with type: int dev_err(const struct device *dev, const char *fmt, ...)
