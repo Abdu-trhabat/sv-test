7571a073384457fe135444e3cb53bd9c881abdcc
erya_wang <eryawang620@gmail.com>
2025-05-26 16:29:04 +0200

    add license header

--- c/unspecified-compile-global-variables/undetected_subexpression_lhs_global.c
+++ c/unspecified-compile-global-variables/undetected_subexpression_lhs_global.c
@@ -1,4 +1,9 @@
-#include <stdlib.h>
+// This file is part of the SV-Benchmarks collection of verification tasks:
+// https://gitlab.com/sosy-lab/benchmarking/sv-benchmarks
+//
+// SPDX-FileCopyrightText: 2007-2023 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0#include <stdlib.h>
 
 extern void abort(void);
 extern void __assert_fail(const char *, const char *, unsigned int, const char *) __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
