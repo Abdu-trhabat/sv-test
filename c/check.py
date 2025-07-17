461af305ff608742490545807878c39e3f18a200
BaierD <daniel.baier@sosy.ifi.lmu.de>
2025-07-17 11:52:19 +0200

    Make allowed file endings in checks a triple

--- c/check.py
+++ c/check.py
@@ -43,7 +43,7 @@ EXPECTED_FILE_PATTERN = re.compile(
 CONFIG_KEYS = set(["Architecture", "Description"])
 PROPERTIES = set(["def-behavior", "no-overflow", "no-data-race", "termination", "unreach-call", "valid-deref", "valid-free", "valid-memcleanup", "valid-memsafety", "valid-memtrack",
     "coverage-error-call", "coverage-branches", "coverage-conditions", "coverage-statements", "unreach-call-a", "unreach-call-b"])
-ALLOWED_INPUT_FILE_ENDINGS = ['.c', '.i', '.yml']
+ALLOWED_INPUT_FILE_ENDINGS = ('.c', '.i', '.yml')
 # multiple properties for eca-rers2018-files
 for i in range(100):
     PROPERTIES.add("unreach-call-%d" % i)
