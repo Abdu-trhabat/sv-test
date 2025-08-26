86a04278e51bff2f3e1bdb96d9a44642a0b8a2b3
Charles Moloney <charles.moloney@hudl.com>
2025-08-26 13:31:33 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Simmo Saan <simmo.saan@gmail.com>

--- java/argv-tasks/HebrewDate/Main.java
+++ java/argv-tasks/HebrewDate/Main.java
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: https://github.com/hebcal/hebcal-java
-// SPDX-License-Identifier: GPL-2.0-only
+// SPDX-License-Identifier: GPL-2.0-or-later
 // SPDX-FileCopyrightText: Copyright (C) 2025 The ARG-V Project
 /** filtered and transformed by ARG-V */
 import org.sosy_lab.sv_benchmarks.Verifier;
