8f0316c6a82ba309cb3be8d3527ac1b671c37556
Raphaël Monat <raphael.monat@inria.fr>
2026-07-17 14:42:44 +0200

    Add original licensing informations

--- python/pyperformance/bm_float_det.py
+++ python/pyperformance/bm_float_det.py
@@ -1,3 +1,10 @@
+# This file is part of the SV-Benchmarks collection of verification tasks:
+# https://gitlab.com/sosy-lab/benchmarking/sv-benchmarks
+#
+# SPDX-FileCopyrightText: 2026 Python Software Foundation
+#
+# SPDX-License-Identifier: MIT
+
 """
 Artificial, floating point-heavy benchmark originally used by Factor.
 """
