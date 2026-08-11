671d40d8a55e1a7928791cd394f11a2f5835e549
Po-Chun Chien <po-chun.chien@sosy.ifi.lmu.de>
2026-08-11 16:24:33 +0200

    Fix CI pipeline
    - add missing license
    - Adjust Makefile such that unsigned overflow is not flagged as error

--- c/firmware-common-simple/array_between_bitfields.c
+++ c/firmware-common-simple/array_between_bitfields.c
@@ -1,3 +1,10 @@
+// This file is part of the SV-Benchmarks collection of verification tasks:
+// https://gitlab.com/sosy-lab/benchmarking/sv-benchmarks
+//
+// SPDX-FileCopyrightText: 2026 The SV-Benchmarks Community
+//
+// SPDX-License-Identifier: Apache-2.0
+
 void reach_error() {}
 
 typedef struct {
