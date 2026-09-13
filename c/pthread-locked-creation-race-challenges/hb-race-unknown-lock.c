bb4423f67230fd237fcf8c50c9aac503b4bfc013
dabund24 <dev@dabund24.de>
2026-09-13 20:28:40 +0200

    locked creation tasks

---
+++ c/pthread-locked-creation-race-challenges/hb-race-unknown-lock.c
@@ -0,0 +1,25 @@
+// verdict: no-data-race false
+// PARAM: --set ana.activated[+] threadJoins --set ana.activated[+] threadDescendants --set ana.activated[+] mustlockHistory --set ana.activated[+] descendantLockset
+#include <pthread.h>
+
+void *__VERIFIER_nondet_pointer(void);
+
+int global = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_t id1;
+
+void *t1(void *arg) {
+  pthread_mutex_t *mutex_ref = __VERIFIER_nondet_pointer(); // unknown mutex
+  pthread_mutex_lock(mutex_ref); // lock of unknown mutex
+  pthread_mutex_unlock(mutex_ref);
+  global++; // RACE!
+  return NULL;
+}
+
+int main(void) {
+  pthread_mutex_lock(&mutex);
+  pthread_create(&id1, NULL, t1, NULL);
+  global++; // RACE!
+  pthread_mutex_unlock(&mutex);
+  return 0;
+}
